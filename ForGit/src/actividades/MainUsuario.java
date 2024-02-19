6fd9cd866144185ea8d65ff3a9742ae784cc777b
FelipeGarcia <felipe.garcia.costa@bootcamp-upgrade.com>
2024-02-19 14:02:56 +0100

    Felipe agrego 2 ultimos metodos

--- ForGit/src/actividades/MainUsuario.java
+++ ForGit/src/actividades/MainUsuario.java
@@ -2,4 +2,24 @@ package actividades;
 
 public class MainUsuario {
 
+	
+	
+	public int devolverCantValoracionesSuperioresa(int numero) {
+		int cantSup = 0;
+		for(int i = 0; i < this.valoraciones.length; i++) {
+			if(numero < this.valoraciones[i]) {
+				cantSup++;
+			}
+		}
+		return cantSup;
+	}
+
+	public boolean devolverSuperaMedia(int numero) {
+		boolean esMayorMedia = false;
+		if(numero < devolverMediaValoraciones() ) {
+			esMayorMedia = true;
+		}
+		return esMayorMedia;
+	}
 }
+
