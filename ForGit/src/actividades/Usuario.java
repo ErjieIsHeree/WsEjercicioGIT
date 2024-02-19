002f7d5ad1a726242eb6da7f4f9c4aa2d01ffe11
AnthonyGu <ninfrad23@gmail.com>
2024-02-19 13:57:29 +0100

    add dos metodos

--- ForGit/src/actividades/Usuario.java
+++ ForGit/src/actividades/Usuario.java
@@ -1,5 +1,25 @@
 package actividades;
 
 public class Usuario {
+	
+	
+	public void motrarValoraciones() {
+		
+		for(int i = 0;i < this.valoraciones.length ;i++) {
+			System.out.println("Lista de valoraciones");
+			System.out.println("Valoracion  " + i + " : " +  this.valoraciones[i]);
+			
+		}
+	}
+	
+	public int devolverPuntuacionesMasAltas(double nota) {
+		int notas_mayores = 0;
+		for (double d : valoraciones) {
+			if (d>nota) {
+				notas_mayores++;
+			}
 
+		}
+		return notas_mayores;
+	}
 }
